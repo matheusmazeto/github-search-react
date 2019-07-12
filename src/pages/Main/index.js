2ab435a3d0ce40dede96a0edd70b1dde51d8f1a4
Matheus Mazeto <mgmazeto@gmail.com>
2019-07-11 22:17:16 -0300

    Add styles to Main page and fetching repositories names from github api

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { Component } from "react";
+import { FaGithubAlt, FaPlus, FaSpinner } from "react-icons/fa";
 
-function Main() {
-  return <div>Main</div>;
+import { api } from "../../services/api";
+
+import { Container, Form, SubmitButton } from "./styles";
+
+class Main extends Component {
+  state = {
+    newRepo: "",
+    repositories: [],
+    loading: false
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    const { newRepo, repositories, loading } = this.state;
+
+    this.setState({ loading: true });
+
+    const response = await api.get(`repos/${newRepo}`);
+
+    const data = {
+      nome: response.data.full_name
+    };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: "",
+      loading: false
+    });
+  };
+
+  render() {
+    const { newRepo, loading } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#fff" size={14} />
+            ) : (
+              <FaPlus color="#fff" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
 
 export default Main;
