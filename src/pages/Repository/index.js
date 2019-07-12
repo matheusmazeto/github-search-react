279cfe6dbeee34e470a37bc7151e0c156797e6b4
Matheus Mazeto <mgmazeto@gmail.com>
2019-07-11 23:13:22 -0300

    Show repository details

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-function Repository({ match }) {
-  return <div>Repository: {decodeURIComponent(match.params.repository)}</div>;
+import { api } from "../../services/api";
+
+import Container from "../../components/Container";
+import { Loading, Owner } from "./styles";
+
+class Repository extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        repository: PropTypes.string
+      })
+    }).isRequired
+  };
+
+  state = {
+    repository: {},
+    issues: [],
+    loading: true
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+
+    const repositoryName = decodeURIComponent(match.params.repository);
+
+    const [repository, issues] = await Promise.all([
+      api.get(`/repos/${repositoryName}`),
+      api.get(`/repos/${repositoryName}/issues`, {
+        params: {
+          state: "open",
+          per_page: 5
+        }
+      })
+    ]);
+
+    this.setState({
+      repository: repository.data,
+      issues: issues.data,
+      loading: false
+    });
+  }
+
+  render() {
+    const { repository, issues, loading } = this.state;
+
+    if (loading) {
+      return <Loading>Carregango...</Loading>;
+    }
+
+    return (
+      <Container>
+        <Owner>
+          <Link to="/">Voltar aos repositórios</Link>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <h1>{repository.name}</h1>
+          <p>{repository.description}</p>
+        </Owner>
+      </Container>
+    );
+  }
 }
 
 export default Repository;
